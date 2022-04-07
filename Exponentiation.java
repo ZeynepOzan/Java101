f4fcf68c29571a3b14019fab1bfd45813b5af47a
ZeynepOzan <ozanzzeynep@gmail.com>
2022-04-07 07:35:19 +0300

    changed

---
+++ Exponentiation.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Numbers {
+	/*
+	 * 1.Java döngüler ile girilen sayıya kadar olan 2'li kuvvetlerini ekrana
+	 * yazdıran programı yazınız. 2.Girilen sayıya kadar olan 4 ve 5'in kuvvetlerini
+	 * ekrana yazdıran program
+	 * 
+	 */
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Sınır değerini giriniz: ");
+		int number = scanner.nextInt();
+		/*
+		 * int t = 2; while (t < number) { System.out.println(t); t *= 2; }
+		 */
+		int t = 4;
+		while (t < number) {
+
+			System.out.println("t: " + t);
+
+			t *= 4;
+		}
+	}
+}
